e1c333048f44ff45c04ab6a6e7ae195ba580c7f8
skyhoffert <skwylarh@yahoo.com>
2022-10-21 09:09:17 -0400

    Now compiling instead of interpreting.

--- soxc.c
+++ soxc.c
@@ -6,16 +6,20 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
+
+#define TMP_FILE ".sox_tmp.c"
 
 int main(int argc, char* argv[])
 {
     //////// Vars ////////
-    char outfile[1024];
+    char outfile[128];
     strcpy(outfile, "a.out");
-    char infiles[8][1024];
+
+    char infiles[8][128];
     for (int i = 0; i < 8; i++)
     {
-        memset(infiles[i], 0, 1024);
+        memset(infiles[i], 0, 128);
     }
     int n_infiles = 0;
 
@@ -63,13 +67,17 @@ int main(int argc, char* argv[])
 
     //////// Parse the file ////////
     FILE* fp = fopen(infiles[0], "r");
-    FILE* fout = fopen(outfile, "w");
+    FILE* fout = fopen(TMP_FILE, "w");
+
+    char maincode[10240];
+    memset(maincode, 0, 10240);
+    int maincodep = 0;
+
+    char include_stdio = 0;
 
     char c = fgetc(fp);
     char buf[1024];
     memset(buf, 0, 1024);
-    char buf2[1024];
-    memset(buf2, 0, 1024);
     int bufp = 0;
 
     int readstate = 0;
@@ -84,41 +92,16 @@ int main(int argc, char* argv[])
 
                 if (strncmp(buf, "stdout", 6) == 0)
                 {
+                    include_stdio = 1;
+
                     char* start = strstr(buf, "\"");
                     start++;
                     char* end = strstr(start, "\"");
                     int len = end - start;
-                    memset(buf2, 0, 1024);
-                    memcpy(buf2, start, len);
-
-                    char buf2c= buf2[0];
-                    int buf2p = 1;
-                    int buf2state = 0;
-                    while (buf2c != 0)
-                    {
-                        if (buf2state == 0)
-                        {
-                            if (buf2c == '\\')
-                            {
-                                buf2state = 1;
-                            }
-                            else
-                            {
-                                printf("%c", buf2c);
-                            }
-                        }
-                        else if (buf2state == 1)
-                        {
-                            if (buf2c == 'n')
-                            {
-                                printf("\n");
-                            }
-                            buf2state = 0;
-                        }
-
-                        buf2c = buf2[buf2p];
-                        buf2p++;
-                    }
+                    maincodep += sprintf(maincode + maincodep, "printf(\"");
+                    memcpy(maincode + maincodep, start, len);
+                    maincodep += len;
+                    maincodep += sprintf(maincode + maincodep, "\");\n");
                 }
             }
             memset(buf, 0, 1024);
@@ -147,7 +130,25 @@ int main(int argc, char* argv[])
     }
 
     fclose(fp);
+
+    if (include_stdio == 1)
+    {
+        fprintf(fout, "#include <stdio.h>\n");
+    }
+
+    fprintf(fout, "int main(int argc, char* argv[])\n{\n");
+
+    fprintf(fout, "%s", maincode);
+
+    fprintf(fout, "return 0;\n}\n");
+
     fclose(fout);
+
+    char soxcomp[256];
+    memset(soxcomp, 0, 256);
+    sprintf(soxcomp, "/usr/bin/gcc -o %s %s", outfile, TMP_FILE);
+    // printf("Running \"%s\"\n", soxcomp);
+    system(soxcomp);
 
     return 0;
 }
