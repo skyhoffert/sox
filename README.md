d3cec900dcac07f7d1e996564f7900f1ca83105c
skyhoffert <skwylarh@yahoo.com>
2022-10-22 14:36:37 -0400

    strings and bools are now implemented.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # sox
 Socket programming language.
+
+## Issues I hope this language solves.
+
+Like C, but TCP and UDP sockets are way easier to work with.
+
--- soxc.c
+++ soxc.c
@@ -8,10 +8,28 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+#define DEBUG 1
+
 #define TMP_FILE ".sox_tmp.c"
+
+#define N_VARS 1024
+#define VAR_NAMELEN 32
+
+#define N_TOKS 8
+#define TOK_NAMELEN 32
+
+#define STR_MAXLEN 1024
+
+int parse_line(char* line, char* toks);
+int parse_string(char* line, char* str);
 
 int main(int argc, char* argv[])
 {
+    if (DEBUG == 1)
+    {
+        printf("//////// soxc DEBUG output ////////\n");
+    }
+
     //////// Vars ////////
     char outfile[128];
     strcpy(outfile, "a.out");
@@ -73,14 +91,30 @@ int main(int argc, char* argv[])
     memset(maincode, 0, 10240);
     int maincodep = 0;
 
+    char vars[N_VARS][VAR_NAMELEN];
+    for (int i = 0; i < N_VARS; i++)
+    {
+        memset(vars[i], 0, VAR_NAMELEN);
+    }
+    int n_vars = 0;
+    char bufvar[VAR_NAMELEN];
+    memset(bufvar, 0, VAR_NAMELEN);
+
     char include_stdio = 0;
+    char include_stdint = 0;
+    char include_string = 0;
 
     char c = fgetc(fp);
     char buf[1024];
     memset(buf, 0, 1024);
     int bufp = 0;
 
+    char toks[N_TOKS*TOK_NAMELEN];
+
+    char str[STR_MAXLEN];
+
     int readstate = 0;
+    int linenum = 1;
 
     while (!feof(fp))
     {
@@ -90,23 +124,71 @@ int main(int argc, char* argv[])
             {
                 // printf("line was: \"%s\"\n", buf);
 
-                if (strncmp(buf, "stdout", 6) == 0)
+                if (strncmp(buf, "stdout(", 7) == 0)
                 {
                     include_stdio = 1;
 
-                    char* start = strstr(buf, "\"");
+                    char* start = strstr(buf, "(");
                     start++;
-                    char* end = strstr(start, "\"");
+                    char* end = strstr(start, ")");
                     int len = end - start;
-                    maincodep += sprintf(maincode + maincodep, "printf(\"");
+                    maincodep += sprintf(maincode + maincodep, "printf(");
                     memcpy(maincode + maincodep, start, len);
                     maincodep += len;
-                    maincodep += sprintf(maincode + maincodep, "\");\n");
+                    maincodep += sprintf(maincode + maincodep, ");\n");
+                }
+                else if (strncmp(buf, "stderr(", 7) == 0)
+                {
+                    include_stdio = 1;
+
+                    char* start = strstr(buf, "(");
+                    start++;
+                    char* end = strstr(start, ")");
+                    int len = end - start;
+                    maincodep += sprintf(maincode + maincodep, "fprintf(stderr, \"\\e[01;31m\");\n");
+                    maincodep += sprintf(maincode + maincodep, "fprintf(stderr, ");
+                    memcpy(maincode + maincodep, start, len);
+                    maincodep += len;
+                    maincodep += sprintf(maincode + maincodep, ");\n");
+                    maincodep += sprintf(maincode + maincodep, "fprintf(stderr, \"\\e[0m\");\n");
+                }
+                else if (strncmp(buf, "int8 ", 4) == 0)
+                {
+                    include_stdint = 1;
+
+                    int ntoks = parse_line(buf, toks);
+
+                    maincodep += sprintf(maincode + maincodep, "int8_t %s = %s;\n", toks+(1*TOK_NAMELEN), toks+(3*TOK_NAMELEN));
+                }
+                else if (strncmp(buf, "string ", 7) == 0)
+                {
+                    include_string = 1;
+
+                    int ntoks = parse_line(buf, toks);
+
+                    int slen = parse_string(buf, str);
+
+                    maincodep += sprintf(maincode + maincodep, "char %s[%d];\n", toks+(1*TOK_NAMELEN), STR_MAXLEN);
+                    maincodep += sprintf(maincode + maincodep, "strcpy(%s, \"%s\");\n", toks+(1*TOK_NAMELEN), str);
+                }
+                else if (strncmp(buf, "bool ", 5) == 0)
+                {
+                    int ntoks = parse_line(buf, toks);
+
+                    maincodep += sprintf(maincode + maincodep, "char %s = %s;\n", toks+(1*TOK_NAMELEN), toks+(3*TOK_NAMELEN));
+                }
+                else
+                {
+                    printf("ERR. Bad line %d \"%s\".\n", linenum, buf);
+                    fclose(fp);
+                    fclose(fout);
+                    return 102;
                 }
             }
             memset(buf, 0, 1024);
             bufp = 0;
             readstate = 0;
+            linenum++;
         }
         else if (readstate == 1) {}
         else
@@ -131,24 +213,95 @@ int main(int argc, char* argv[])
 
     fclose(fp);
 
+    //////// Parse maincode and replace as needed. ////////
+    char* bfinder = strstr(maincode, "\%b");
+    while (bfinder != NULL)
+    {
+        // Replace "%b" with "%d".
+        bfinder[1] = 'd';
+        bfinder = strstr(bfinder+1, "\%b");
+    }
+
+    //////// Print to C file. ////////
     if (include_stdio == 1)
     {
         fprintf(fout, "#include <stdio.h>\n");
     }
+    if (include_stdint == 1)
+    {
+        fprintf(fout, "#include <stdint.h>\n");
+    }
+    if(include_string == 1)
+    {
+        fprintf(fout, "#include <string.h>\n");
+    }
 
     fprintf(fout, "int main(int argc, char* argv[])\n{\n");
-
     fprintf(fout, "%s", maincode);
-
     fprintf(fout, "return 0;\n}\n");
-
     fclose(fout);
 
     char soxcomp[256];
     memset(soxcomp, 0, 256);
     sprintf(soxcomp, "/usr/bin/gcc -o %s %s", outfile, TMP_FILE);
     // printf("Running \"%s\"\n", soxcomp);
-    system(soxcomp);
+    int retval = system(soxcomp);
+
+    if (retval == 0 && DEBUG != 1)
+    {
+        remove(TMP_FILE);
+    }
+    
+    if (DEBUG == 1)
+    {
+        printf("//////// END soxc DEBUG ////////\n\n");
+    }
 
     return 0;
+}
+
+int parse_line(char* line, char* toks)
+{
+    memset(toks, 0, N_TOKS * TOK_NAMELEN);
+    char* tokpa = line;
+    char* tokpb = NULL;
+    int len = 0;
+    int ntoks = 0;
+    do
+    {
+        tokpb = strstr(tokpa+1, " ");
+        if (tokpb == NULL)
+        {
+            tokpb = strstr(tokpa+1, ";");
+        }
+        if (tokpb == NULL)
+        {
+            break;
+        }
+
+        len = tokpb - tokpa;
+        memcpy(toks+(ntoks*TOK_NAMELEN), tokpa, len);
+        ntoks++;
+        tokpa = strstr(tokpa+1, " ");
+        if (tokpa != NULL)
+        {
+            tokpa++;
+        }
+    } while (tokpa != NULL);
+
+    return ntoks;
+}
+
+int parse_string(char* line, char* str)
+{
+    memset(str, 0, STR_MAXLEN);
+
+    char* start = strstr(line, "\"");
+    start++;
+    char* end = strstr(start, "\"");
+    int len = end - start;
+
+    memcpy(str, start, len);
+
+    return len;
 }
